b67aedda15eec9474066cd4d91e4065b2eee9e68
Ankit Mishra <mishra.ankit.dev@gmail.com>
2025-06-26 11:02:54 +0530

    Create resizable-panel.component.ts

---
+++ Angular/Directives/Resizer/resizable-panel.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ResizerDirective } from './shared/resizer.directive';
+
+@Component({
+  selector: 'app-resizable-panel',
+  standalone: true,
+  imports: [ResizerDirective],
+  template: `
+    <div
+      resizer
+      style="width: 300px; height: 40px; border: 1px solid #ccc; margin: 2rem 0;"
+    >
+      Drag my edge to resize me!
+    </div>
+  `
+})
+export class ResizablePanelComponent {}
